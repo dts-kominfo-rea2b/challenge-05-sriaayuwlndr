4132da037805ad830c8031eaeb27b73859fec2fd
sriayu <sriaayuwlndr@gmail.com>
2022-06-04 21:45:14 +0700

    Percobaan 1

--- index.js
+++ index.js
@@ -6,15 +6,123 @@ const names = ["Halo", "Angel", "Nyoman", "Ketut", "Aisyah"];
 // "1. Nama"
 // "2. Nama"
 // ...
-const sorter = null;
+
+const sorter = (data, langkahSelanjutnya) => {
+
+  let panjangData = data.length;
+  let resultNamaSorter = [];
+  let resultNamaAsc = [];
+  let nama = [];
+  let urutanNomor = 0;
+
+  for (let i = 0; i < panjangData; i++)
+  {
+      nama[i] = data[i];
+      urutanNomor += 1;
+      
+      resultNamaSorter[i] = urutanNomor+" . "+nama[i];
+      resultNamaAsc[i] = nama[i]; 
+  }
+
+  // langkahSelanjutnya(data.sort());
+
+  if(langkahSelanjutnya == sortAscending)
+  {
+    langkahSelanjutnya(data.sort());
+  }
+
+  else if(langkahSelanjutnya == sortDescending)
+  {
+    langkahSelanjutnya(data.reverse());
+  }
+
+  // return resultNamaSorter;
+}
+
+// console.log(sorter(names));
+
+// const sorter = (data) => {
+
+//   let panjangData = data.length;
+//   let resultNamaSorter = [];
+//   let resultNamaAsc = [];
+//   let nama = [];
+//   let urutanNomor = 0;
+
+//   for (let i = 0; i < panjangData; i++)
+//   {
+//       nama[i] = data[i];
+//       urutanNomor += 1;
+      
+//       resultNamaSorter[i] = urutanNomor+" . "+nama[i];
+//       resultNamaAsc[i] = nama[i];
+//   }
+
+
+//   return {resultNamaSorter, resultNamaAsc};
+
+// }
+
+// console.log(sorter(names));
+
+
 
 // TODO: Fungsi untuk mengurutkan array of string secara ascending
 // mengembalikan array yang sudah diurutkan
-const sortAscending = null;
+const sortAscending = (data) => {
+
+  let panjangData = data.length;
+  let resultNamaSorter = [];
+  let resultNamaAsc = [];
+  let nama = [];
+  let urutanNomor = 0;
+
+  for (let i = 0; i < panjangData; i++)
+  {
+      nama[i] = data[i];
+      urutanNomor += 1;
+      
+      resultNamaSorter[i] = urutanNomor+" . "+nama[i];
+      // resultNamaAsc[i] = nama[i]; 
+  }
+
+  console.log(resultNamaSorter);
+  // return resultNamaSorter;
+}
+
+const sortDescending = (data) => {
+
+  let panjangData = data.length;
+  let resultNamaSorter = [];
+  let resultNamaAsc = [];
+  let nama = [];
+  let urutanNomor = 0;
+
+  for (let i = 0; i < panjangData; i++)
+  {
+      nama[i] = data[i];
+      urutanNomor += 1;
+      
+      resultNamaSorter[i] = urutanNomor+" . "+nama[i];
+      // resultNamaAsc[i] = nama[i]; 
+  }
+
+  console.log(resultNamaSorter);
+  // return resultNamaSorter;
+}
+
+// console.log(sorter(names, sortAscending));
+// // console.log(sorter(names, sortAscending));
+// console.log(sorter(names, sortDescending));
+
+// const sortDescending = (data) => {
+//   return data;
+// }
+
 
 // TODO: Fungsi untuk mengurutkan array of string secara descending
 // mengembalikan array yang sudah diurutkan
-const sortDescending = null;
+// const sortDescending = null;
 
 // ! JANGAN DIMODIFIKASI
 (function main() {
